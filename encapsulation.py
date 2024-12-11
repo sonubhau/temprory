8d67c2b08637258c37feb304fd7df625b2c832ec
sonu <t.y.katre@gmail.com>
2024-12-11 11:29:41 +0530

    modification in encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -7,13 +7,16 @@ class Car:
     def __init__(self,brand,model):
         self.__brand = brand
         self.model = model
-    def get_brand(self):
+    def get_brand(self): # getter Method
         return self.__brand + "!"
+    def set_brand(self,brand_name): # setter Method
+        self.__brand =  brand_name
     def full_name(self):
         return f"{self.__brand} - {self.model}"
 c1 = Car("Tata","Nexon")
 print(c1.full_name())
 # print(c1.__brand) Its not accesseble as a varible at instance
+c1.set_brand("Suzuki")
 print(c1.get_brand())
 print(c1.model)
 
